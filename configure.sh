dbafd04fcc570a5b13a5e9463ab2dbac5e29c593
Kerry Snyder <krry@me.com>
2019-02-24 04:38:48 -0600

    make sure to switch to zsh before running install

--- configure.sh
+++ configure.sh
@@ -43,25 +43,30 @@ if ! curl "$URL" --silent --output "$HOME/.git-completion.bash" ; then
   echo "ERROR: Couldn't download completion script. Make sure you have a working internet connection." && exit 1
 fi
 
-# prezto zsh install
-if [ -d ~/.zprezto/ ] ; then
-  echo "prezto is already installed."
-  vared -cp "Would you like to update prezto now?" REPLY
-  if [[ $REPLY =~ ^[Yy]$ ]] ; then
-    zprezto-update
-  fi
-elif type zsh >/dev/null 2>&1
+if type zsh >/dev/null 2>&1 ; then
   echo "switching to zsh"
   exec zsh
-  echo "installing prezto for zsh..."
-  git clone -q --recursive https://github.com/krry/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-  setopt EXTENDED_GLOB
-  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-  done
- else
+  # prezto zsh install
+  if [ -d ~/.zprezto/ ] ; then
+    echo "prezto is already installed."
+    vared -cp "Would you like to update prezto now?" REPLY
+    if [[ $REPLY =~ ^[Yy]$ ]] ; then
+      zprezto-update
+    fi
+  else
+    echo "installing prezto for zsh..."
+    git clone -q --recursive https://github.com/krry/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+    setopt EXTENDED_GLOB
+    for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+      ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    done
+  fi
+else
   echo "zsh not found, so no prezto change-o for you"
+  exit
 fi
+
+
 
 # Pull down personal dotfiles
 vared -cp "Do you want to use krry's dotfiles? y/n" REPLY
