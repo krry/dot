3a93823bc93e17bbb642c2c27e085d6067c71755
Kerry Snyder <krry@me.com>
2019-02-27 00:09:17 -0600

    cleaner, slimmer, simpler, DRYer

--- liftoff.sh
+++ liftoff.sh
@@ -52,20 +52,21 @@ PROMPT_URL="https://raw.githubusercontent.com/git/git/v$GIT_VERSION/contrib/comp
 pfinfo "Downloading git-prompt git@v$GIT_VERSION..."
 $(curl "$PROMPT_URL" --silent --output "$HOME/.git-prompt.sh") || pffail "Couldn't download prompt  script."
 
+DOT_DIR="$HOME/.dotfiles"
 # backup current dotfiles
 if [[ -d ~/.dotfiles ]] ; then
     pfwarn "existing dotfiles found"
     now="$(date '+%Y%m%d%H%M')"
     pfinfo "Backing up old dotfiles in ~/.dotfiles_$now"
-    mv "$HOME/.dotfiles" "$HOME/.dotfiles_$now"
+    mv "$DOT_DIR" "$DOT_DIR_$now"
 fi
 
 # clone fresh dotfiles
-pfinfo "cloning krry's dotfiles to $HOME/.dotfiles"
-git clone https://github.com/krry/dot.git $HOME/.dotfiles
+pfinfo "cloning krry's dotfiles to $DOT_DIR"
+git clone https://github.com/krry/dot.git $DOT_DIR
 pfinfo "importing submodules"
-cd $HOME/.dotfiles
+cd $DOT_DIR
 git submodule init
 git submodule update --remote
 pfinfo "Installing krry's dotfiles"
-$HOME/.dotfiles/script/bootstrap
+$DOT_DIR/bootstrap
